475c4602e12afddc7040564910f017d34d1f9c8c
Ben Hyde <bhyde@pobox.com>
2017-06-25 16:54:02 -0400

    Initial version, might work.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # less-file
 Emac command to run less in an ansi-term on a pathname, including tramp pathnames.
+
+We rearly need or want's a pager in Emacs.  But, I do because I often need to look into vast log files that
+are easily accesible via tramp on some remote server.
+
+So given a file name (tramp or not) this will create a ansi-term window running less on the remote machine.
+
+To kill the ansi term window try: C-x k, just like other buffers.
+
+Lots of improvements are possible, but who knows what the future holds.
+
+My init file loads this as so:
+
+```
+(use-package less-file
+   :ensure
+   :commands less-file
+   :quelpa (less-file :fetcher github :repo "bhyde/less-file"))
+```
+
---
+++ less-file.el
@@ -0,0 +1,48 @@
+;;; less-file.el -- Given a pathname run less in a line mode ansi-term.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017 Ben Hyde
+
+;; Author: Ben Hyde
+;; Keywords: lisp
+;; Version: 0.0.1
+
+;; License:
+
+;; Apache 2.0
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'tramp)
+(require 'term)
+
+(defun less-file (pathname)
+  "Open a line-mode 'ansi-term' running unix less on the given PATHNAME."
+  (interactive "fpathname:")
+  (cond
+   ((tramp-tramp-file-p pathname)
+    (with-parsed-tramp-file-name pathname f
+      (let ((buff (term-ansi-make-term "*log-view*" "/bin/bash")))
+        (set-buffer buff)
+        (switch-to-buffer buff)
+        (term-mode)
+        (term-line-mode)
+        (cl-flet ((f (fmt &rest args)
+                     (let ((x (apply #'format fmt args)))
+                       (message x)
+                       (term-send-string buff x))))
+          (f "ssh %s\n" f-host)
+          (sleep-for 1)
+
+          (f "%sless %s\n" (if (string= f-method "sudo") "sudo " "") f-localname)))))
+   (t
+    (let ((buff (term-ansi-make-term "*log-view*" "/bin/bash")))
+      (set-buffer buff)
+      (switch-to-buffer buff)
+      (term-mode)
+      (term-line-mode)
+      (term-send-string buff (format "less %s\n" pathname))))))
+
+(provide 'less-file)
+;;; less-file.el ends here
